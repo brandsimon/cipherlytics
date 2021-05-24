c633e77f7254ea423ddcd637579bec7fa1da5bb3
Simon Brand <simon.brand@postadigitale.de>
2021-05-24 22:53:18 +0000

    Use argument parser in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
+use std::env;
+mod analytics;
 mod input;
 mod types;
+mod arguments;
 
 fn main() {
-	let vec = input::read_file("src/main.rs_printable").unwrap();
-	let vec2 = input::convert_vec::<u16>(&vec).unwrap();
-	let (_head, body, _tail) = unsafe { vec2.align_to::<u8>() };
-	println!("{}", String::from_utf8(vec).unwrap());
-	println!("{}", String::from_utf8_lossy(body));
+	let args: Vec<String> = env::args().collect();
+	let action = match arguments::parse_args(&args) {
+		Ok(l) => l,
+		Err(s) => {
+			println!("{}", s);
+			return;
+		},
+	};
+	if action.help {
+		println!("Help message"); // TODO
+		return;
+	}
+	let input = input::read_file(&action.filename).unwrap();
+	// TODO: for u8 ... u128
+	let vec = input::convert_vec::<u8>(&input).unwrap();
+
+	match action.method {
+		arguments::AnalyzeMethod::None => (),
+		arguments::AnalyzeMethod::MinMax => {
+			let res = analytics::min_max(&vec).unwrap();
+			println!("Min: {}, Max: {}", res.0, res.1);
+		},
+		arguments::AnalyzeMethod::FrequencyAnalysis => {
+			let res = analytics::frequency_analysis(&vec);
+			for (i, j) in res {
+				println!("{}: {}", j, i);
+			}
+		},
+		arguments::AnalyzeMethod::KasiskiExamination(l) => {
+			let res = analytics::kasiski_examination(&vec, l);
+			println!("Res size: {}", res.len());
+			for (i, j) in res {
+				println!("{:?}: {:?}", j, i);
+			}
+		},
+	};
 }
